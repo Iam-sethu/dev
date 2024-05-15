5703258d8abe0def08a02118ba26e48907d58a2c
Iam-sethu <137145648+Iam-sethu@users.noreply.github.com>
2024-05-15 19:30:41 +0530

    Update jenkins-install.sh

--- jenkins/jenkins-install.sh
+++ jenkins/jenkins-install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-# Java installation for jenkins
 
-sudo apt update
-sudo apt install openjdk-11-jre -y
+# install jenkins
+sudo yum update -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
 
-# Jenkins installation 
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins -y
+# then install git
+sudo yum install git -y
 
 #then install terraform
 sudo yum install -y yum-utils
@@ -21,11 +22,4 @@ sudo yum -y install terraform
 #finally install kubectl
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/
-
-
-# Install AWS CLI 
-sudo apt install unzip 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
